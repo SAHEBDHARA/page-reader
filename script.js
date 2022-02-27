6b6844abaaa9075f2ffcf4dd5aaf7d758b6423fa
Saheb Dhara <sahebdhara961@gmail.com>
2022-02-27 17:53:15 +0530

    second commit

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const video = document.querySelector("video")
-const textElem = document.querySelector("[data-text]")
+const video = document.querySelector("video") // 
+const textElem = document.querySelector("[data-text]") // select the text recognized from the canvas
 
-async function setup() {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-  video.srcObject = stream
+async function setup() { // making that async  
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true }) // geting the acces of users video access
+  video.srcObject = stream // use stream as the source of the video
 
-  video.addEventListener("playing", async () => {
-    const worker = Tesseract.createWorker()
-    await worker.load()
-    await worker.loadLanguage("eng")
-    await worker.initialize("eng")
+  video.addEventListener("playing", async () => { // if run or pause the video, this function will activate
+    const worker = Tesseract.createWorker() // that is giong to create a worker which is comming from the source link
+    await worker.load() // this is going to load our worker
+    await worker.loadLanguage("eng") // then load the language in case the language is english
+    await worker.initialize("eng") // we have initialize with that language 
 
-    const canvas = document.createElement("canvas")
-    canvas.width = video.width
-    canvas.height = video.height
+    const canvas = document.createElement("canvas") // used canvas to get img from the video 
+    canvas.width = video.width // cnavas width 
+    canvas.height = video.height // canvas height
 
-    document.addEventListener("keypress", async e => {
-      if (e.code !== "Space") return
-      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
-      const {
+    document.addEventListener("keypress", async e => { // this event will listne while pressing any key
+      if (e.code !== "Space") return // if the keypress is space then return..
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height) // draw the entair video screen over the enteir canvas
+      const {                                                                   // this gives the current frame of our video into the canvas
         data: { text },
-      } = await worker.recognize(canvas)
+      } = await worker.recognize(canvas) // text recognize from the canvas
 
-      speechSynthesis.speak(
-        new SpeechSynthesisUtterance(text.replace(/\s/g, " "))
+      speechSynthesis.speak( // speechSynthesis API is used to speak the text 
+        new SpeechSynthesisUtterance(text.replace(/\s/g, " ")) // avoiding the space newline 
       )
 
-      textElem.textContent = text
+      textElem.textContent = text // save the text to the textElem
     })
   })
 }
